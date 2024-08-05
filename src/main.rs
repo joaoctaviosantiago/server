6d8be1287408a995d6a0f62fbb70a91b5d861e69
João Octávio Santiago <jomsoutosantiago@gmail.com>
2024-08-05 04:16:19 -0300

    created a connection handler; added the 200 OK response;

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
-use std::net::TcpListener;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
 
-        println!("Connection stabilished!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer: [u8; 1024] = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
